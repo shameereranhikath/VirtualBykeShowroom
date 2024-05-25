e504ec695834d3ed83121fc169b1b81c4d73fcda
ShameerEranhikath <shmr_ekth@yahoo.com>
2024-05-25 17:15:09 +0530

    Initial Commit- Solution file is created

---
+++ VirtualBykeStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VirtualBykeStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
